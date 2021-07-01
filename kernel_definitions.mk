0090af7b061bb92d8735300fc750bb8f1ba0e59c
Venkata Rao Kakani <vkakani@codeaurora.org>
2021-07-01 23:42:59 +0530

    build: Create dummy vendor_boot.img
    Create dummy vendor boot image if DUMMY_VENDOR_BOOT is
    defined.
    
    Signed-off-by: Venkata Rao Kakani <vkakani@codeaurora.org>

--- kernel_definitions.mk
+++ kernel_definitions.mk
@@ -303,3 +303,7 @@ $(RTIC_DTB): $(INSTALLED_KERNEL_TARGET)
 # Creating a dtb.img once the kernel is compiled if TARGET_KERNEL_APPEND_DTB is set to be false
 $(INSTALLED_DTBIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
 	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(RTIC_DTB) > $@
+
+ifeq ($(TARGET_DUMMY_VENDOR_BOOT), true)
+	$(shell dd if=/dev/zero of=$(PRODUCT_OUT)/vendor_boot.img bs=1M count=96)
+endif
